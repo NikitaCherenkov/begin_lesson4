6ff4d4f10f5564385bf2f5e217cea99a6c5e5e7e
Nikita <cherenkov204@gmail.com>
2022-03-27 13:52:36 +0300

    tasks done

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+string entry = Convert.ToString(Console.ReadLine());
+int num;
+int sum = 0;
+bool isNum = int.TryParse(entry, out num);
+num = Math.Abs(num);
+if (isNum) {
+    while (num > 0) {
+        sum = sum + num % 10;
+        num = num / 10;
+        Console.WriteLine($"{sum}");
+    }
+    Console.WriteLine($"Сумма цифр в числе: {sum}");
+} else Console.WriteLine("Введено не число");
---
+++ task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+string[] array = new string[8];
+for (int i = 0; i < array.Length; i++) {
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    array[i] = Convert.ToString(Console.ReadLine());
+}
+for (int n = 0; n < array.Length; n++) {
+    if (n < array.Length - 1) Console.Write($"{array[n]}; "); else Console.Write($"{array[n]}");
+}
