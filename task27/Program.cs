7df5b26032959fc95bdf0e4e2a487e83c16d6056
Nikita <cherenkov204@gmail.com>
2022-03-27 15:33:27 +0300

    task27 done

--- task27/Program.cs
+++ task27/Program.cs
@@ -10,7 +10,6 @@ if (isNum) {
     while (num > 0) {
         sum = sum + num % 10;
         num = num / 10;
-        Console.WriteLine($"{sum}");
     }
     Console.WriteLine($"Сумма цифр в числе: {sum}");
 } else Console.WriteLine("Введено не число");
