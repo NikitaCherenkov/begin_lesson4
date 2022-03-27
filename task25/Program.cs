e7910bd36309505e8f2327bf1c4f24c9298724e2
Nikita <cherenkov204@gmail.com>
2022-03-27 16:04:45 +0300

    task25 update

--- task25/Program.cs
+++ task25/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Введите число A");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int B = Convert.ToInt32(Console.ReadLine());
+B = Math.Abs(B);
 int result = A;
 if (B > 1) {
     for (int i = 1; i < B; i++) result = result * A;
