1b5122d3f5a725f169bf8a91c004f6cf0a3c7ff7
Nikita <cherenkov204@gmail.com>
2022-03-27 15:30:55 +0300

    rewrite task25

--- task25/Program.cs
+++ task25/Program.cs
@@ -4,5 +4,10 @@ Console.WriteLine("Введите число A");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int B = Convert.ToInt32(Console.ReadLine());
-double result = Math.Pow(A, B);
-Console.WriteLine($"{result}");
+int result = A;
+if (B > 1) {
+    for (int i = 1; i < B; i++) {
+    result = result * A;
+    }
+    Console.WriteLine($"A ^ B = {result}");
+} else Console.WriteLine($"A ^ B = {A}");
